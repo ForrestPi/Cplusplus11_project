c07551a8463c880c05ccd7e771b4bfe51df626fe
Forrest <forrest_zhu@foxmail.com>
2016-06-07 12:25:27 +0800

    Add Slot
    Add Slot

---
+++ ConcurrentSlot/main.cpp
@@ -0,0 +1,40 @@
+#include <concurrent/slot.hpp>
+#include <concurrent/notifier.hpp>
+
+#include <thread>
+
+#include <cstdio>
+#include <cstdlib>
+
+static concurrent::slot<int> input; ///< shared
+static concurrent::notifier notifier; ///< shared
+
+static void worker() {
+    try {
+        for (;;) {
+            int value;
+            input.waitGet(value);
+            printf("worker got : %d\n", value);
+            notifier.ack();
+        }
+    } catch (concurrent::terminated &e) {
+        printf("worker terminates\n");
+    }
+}
+
+int main(int argc, char **argv) {
+    std::thread worker_thread(&::worker);
+
+    for (int i = 0; i < 10; ++i) {
+        printf("main sending : %d\n", i);
+        input.set(i);
+        notifier.wait();
+    }
+
+    printf("main sending termination\n");
+    input.terminate();
+
+    worker_thread.join();
+
+    return EXIT_SUCCESS;
+}
